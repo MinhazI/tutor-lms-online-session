f63905ec8ddef085b4a0156e15141ec43c7045c4
Minhaz Irphan Mohamed <MinhazI@users.noreply.github.com>
2024-02-22 13:14:54 +0530

    Updated the code

--- tutor-lms-online-session.php
+++ tutor-lms-online-session.php
@@ -1,16 +1,32 @@
 <?php
+
+require_once(__DIR__ . '/messages/dashboard-admin-messages.php');
+include_once ABSPATH . 'wp-admin/includes/plugin.php';
+
 /*
-Plugin Name: Custom Tutor LMS Extension
-Description: Adds custom functionality to Tutor LMS.
+Plugin Name: Tutor LMS Online Session Extension
+Description: Adds an online session menu to Tutor LMS and to the WP dashboard with Dyte.
 Version: 1.0
-Author: Your Name
+Author: Win Authority LLC
 */
 
 // Check if Tutor LMS is active
 function is_tutor_lms_active()
 {
-    return class_exists('TutorLMS');
+    return is_plugin_active('tutor/tutor.php');
 }
+
+// Function to run upon plugin activation
+function custom_tutor_lms_extension_activate()
+{
+    $is_tutor_active = is_tutor_lms_active();
+    // Check if Tutor LMS is active
+    if (!$is_tutor_active) {
+        // Tutor LMS is not active, display an error message and halt activation
+        wp_die('Sorry, but this plugin requires Tutor LMS to be installed and active. Please activate Tutor LMS and try again.');
+    }
+}
+register_activation_hook(__FILE__, 'custom_tutor_lms_extension_activate');
 
 // Add custom link to Tutor LMS dashboard navigation
 function add_custom_link_to_dashboard($links)
